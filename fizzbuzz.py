8c931e21a8e4ad45f35f03c610f3792d8f9c1aae
June3723 <juen.m3723@gmail.com>
2025-04-23 11:14:18 +0900

    Refactoring: fizzbuzz sith 1 if statement

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,6 @@
-print ('hello world')
 
-for i in range(1, 16+1):
-    if i % 3 == 0:
-        print('fizz')
+for i in range(1, 19+1):
+    if i % 3 == 0 or i % 5 == 0:
+        print('Fizz' * (i % 3 == 0) + 'Buzz' * (i % 5 == 0))
     else:
         print(f'{i}')
